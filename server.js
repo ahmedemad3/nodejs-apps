29407621a5e8e41efa660f5f79ebb664849dc8fd
ahmedemad3 <ahmedemad3@gmail.com>
2021-05-17 02:09:25 +0300

    implement socket.io

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 
 var app = express();
+const { Server } = require("socket.io");
+
 
 app.use(cors())
 
@@ -24,9 +26,12 @@ app.use(bodyParser.json());
 
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
+// Expose the node_modules folder as static resources (to access socket.io.js in the browser)
+app.use('/static', express.static('node_modules'));
 
 app.get("/" , function(req , res) {
-    res.send("Server started ........");
+    // res.send("Server started ........");
+    res.sendFile(__dirname + '/index.html')
 });
 
 app.use("/api/v1" , noteRoute);
@@ -38,8 +43,40 @@ app.use("/api/v1" , uploadRoute);
 app.use("/api/v1" , exportRoute);
 
 
-app.listen(4000, () => {
+const server= app.listen(4000, () => {
     console.log(`Server start ....... `)
 })
+
+// initialize & listen to server
+const io = new Server(server);
+// Handle connection
+io.on('connection', function (socket) {
+    console.log("Connected successfully to the socket ...");
+    
+    setInterval(function(){
+        var news = getNews();
+        // Send news on the socket
+        socket.emit('news', news);
+    } , 5000);
+    
+
+    socket.on('my other event', function (data) {
+        console.log(data);
+    });
+});
+
+function getNews(){
+    var length = Math.floor(Math.random() * 21);
+    var news = [];
+    for(var i = 0; i < length ; i++ ){
+        var val = {id : i , title : 'The cure of the Sadness is to play Videogames' + i , date: new Date() }
+        news.push(val);
+    }
+    return news
+}
+
+
+
+
 
 module.exports = app
